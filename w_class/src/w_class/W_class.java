f2b739a9a44196a6ea0e0d7a00e42ceb2b8350ce
Nabi jan Aminkhil <nabijanaminkhil@gmail.com>
2022-11-15 09:22:54 +0430

    added w_class

---
+++ w_class/src/w_class/W_class.java
@@ -0,0 +1,21 @@
+
+package w_class;
+
+
+public class W_class {
+
+   
+    public static void main(String[] args) {
+      Float f = new Float(23);
+      float j=f.floatValue();
+       System.out.println(j);
+        
+       
+       Integer i = new Integer(6); 
+        System.out.println(i.intValue());
+        
+        
+        
+    }
+    
+}
