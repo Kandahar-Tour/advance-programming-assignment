717fb9dd7e63a342a92aff037992386b5cb121ab
Nabi jan Aminkhil <nabijanaminkhil@gmail.com>
2022-11-15 08:39:29 +0430

    added abstruction

---
+++ abstractionExample/src/abstractionexample/AbstractionExample.java
@@ -0,0 +1,23 @@
+
+package abstractionexample;
+
+abstract class test{
+    void sum(){
+        System.out.println("Hello World");
+    }
+}
+class test1 extends test{
+        
+    }
+    
+public class AbstractionExample{
+
+  
+    public static void main(String[] args) {
+      test1 t = new test1();
+      t.sum();
+
+       
+    }
+    
+}
